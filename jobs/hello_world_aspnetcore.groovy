e44594ccd47c772b0e502cbffaf27569fd0448ac
Ankur Soni <ankur.soni@outlook.com>
2020-09-26 17:12:13 +1000

    Refactor

--- jobs/hello_world_aspnetcore.groovy
+++ jobs/hello_world_aspnetcore.groovy
@@ -9,6 +9,7 @@ multibranchPipelineJob("hello-world-aspnetcore") {
     configure {
         it / 'factory' << 'scriptPath'('demo-apps/hello-world-aspnetcore/pipeline/Jenkinsfile')
         it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' << 'traits' {
+            'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
             'extension'(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
                 shallow(false)
                 noTags(false)
--- jobs/hello_world_nodejs.groovy
+++ jobs/hello_world_nodejs.groovy
@@ -9,6 +9,7 @@ multibranchPipelineJob("hello-world-nodejs") {
     configure {
         it / 'factory' << 'scriptPath'('demo-apps/hello-world-nodejs/pipeline/Jenkinsfile')
         it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' << 'traits' {
+            'jenkins.plugins.git.traits.BranchDiscoveryTrait' { }
             'extension'(class: 'hudson.plugins.git.extensions.impl.CloneOption') {
                 shallow(false)
                 noTags(false)
