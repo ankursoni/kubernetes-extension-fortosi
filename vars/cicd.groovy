84c1d6a3684db79d6a845fb1dbabe8f667ef4fc1
Ankur Soni <ankur.soni@outlook.com>
2020-10-06 01:12:37 +1100

    Refactor

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -58,7 +58,7 @@ def call(String appName) {
                                 ]
                             ) {
                                 sh "mkdir -p ~/.kube"
-                                sh "cp ${kubeconfig-secret} ~/.kube/config"
+                                sh "cp \${kubeconfig-secret} ~/.kube/config"
                                 sh "cd ${SCRIPT_PATH}; ./kubernetes.sh ${KUBERNETES_NAMESPACE} ${CONTAINER_REGISTRY_URL} ${CONTAINER_REPOSITORY_NAME} ${IMAGE_TAG} ${APP_NAME}"
                             }
                         }
