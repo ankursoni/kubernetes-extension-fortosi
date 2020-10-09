f5062313e9e0e2f7cf6be4635733c3a338205efb
Ankur Soni <ankur.soni@outlook.com>
2020-10-09 21:42:54 +1100

    Refactor

--- infra/aws/aws.tf
+++ infra/aws/aws.tf
@@ -141,7 +141,6 @@ resource "aws_eks_node_group" "eksng01" {
   node_group_name = "${var.prefix}-${var.environment}-eksng01"
   node_role_arn   = aws_iam_role.iamr02.arn
   subnet_ids      = [for s in module.vpc.private_subnets : s]
-  # instance_types  = ["t2.medium"]
 
   scaling_config {
     desired_size = var.node_count
@@ -223,43 +222,43 @@ resource "aws_iam_role_policy_attachment" "iamrpa05" {
 }
 
 
-# resource "aws_eks_fargate_profile" "eksfp01" {
-#   cluster_name           = aws_eks_cluster.eks01.name
-#   fargate_profile_name   = "${var.prefix}-${var.environment}-eksfp01"
-#   pod_execution_role_arn = aws_iam_role.iamr03.arn
-#   subnet_ids             = [for s in module.vpc.private_subnets : s]
+resource "aws_eks_fargate_profile" "eksfp01" {
+  cluster_name           = aws_eks_cluster.eks01.name
+  fargate_profile_name   = "${var.prefix}-${var.environment}-eksfp01"
+  pod_execution_role_arn = aws_iam_role.iamr03.arn
+  subnet_ids             = [for s in module.vpc.private_subnets : s]
 
-#   selector {
-#     namespace = "kube-system"
-#   }
-#   selector {
-#     namespace = "kubernetes-dashboard"
-#   }
-#   selector {
-#     namespace = "default"
-#   }
+  selector {
+    namespace = "kube-system"
+  }
+  selector {
+    namespace = "kubernetes-dashboard"
+  }
+  selector {
+    namespace = "default"
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.iamrpa06
-#   ]
-# }
+  depends_on = [
+    aws_iam_role_policy_attachment.iamrpa06
+  ]
+}
 
-# resource "aws_iam_role" "iamr03" {
-#   name = "${var.prefix}-${var.environment}-iamr03"
+resource "aws_iam_role" "iamr03" {
+  name = "${var.prefix}-${var.environment}-iamr03"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "eks-fargate-pods.amazonaws.com"
-#       }
-#       Action = "sts:AssumeRole"
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "iamrpa06" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-#   role       = aws_iam_role.iamr03.name
-# }
+resource "aws_iam_role_policy_attachment" "iamrpa06" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.iamr03.name
+}
